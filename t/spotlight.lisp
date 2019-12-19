8745e9db72459f1fbe806b6127a7403373226901
Conjunctive <conjunctive@protonmail.com>
2019-12-18 23:23:39 -0800

    v0.2.1

--- t/spotlight.lisp
+++ t/spotlight.lisp
@@ -63,28 +63,28 @@
 
 (subtest "Mutably apply increment function over second value in list"
   (let ((xs (list 1 2 3 4))
-        (fn (lambda (x) (+ x 1)))
+        (fn #'1+)
         (l  (nth! 1)))
     (is (over l fn xs)
         (list 1 3 3 4))))
 
 (subtest "Mutably apply increment function over second value in vector"
   (let ((xs #(1 2 3 4))
-        (fn (lambda (x) (+ x 1))))
+        (fn #'1+))
     (is (over (nth! 1) fn xs)
         #(1 3 3 4)
         :test #'equalp)))
 
 (subtest "Immutably apply increment function over second value in list"
   (let ((xs (list 1 2 3 4))
-        (fn (lambda (x) (+ x 1))))
+        (fn #'1+))
     (is (over (nth- 1) fn xs)
         (list 1 3 3 4))
     (is xs (list 1 2 3 4))))
 
 (subtest "Immutably apply increment function over second value in vector"
   (let ((xs #(1 2 3 4))
-        (fn (lambda (x) (+ x 1))))
+        (fn #'1+))
     (is (over (nth- 1) fn xs)
         #(1 3 3 4)
         :test #'equalp)
@@ -94,8 +94,7 @@
 
 (subtest "Immutably apply increment function over tail of list"
   (let* ((xs  (list 1 2 3 4))
-         (inc (lambda (x) (+ x 1)))
-         (fn  (lambda (xs) (mapcar inc xs))))
+         (fn  (lambda (xs) (mapcar #'1+ xs))))
     (is (over tail fn xs)
         (list 3 4 5))
     (is xs (list 1 2 3 4))))
@@ -108,14 +107,14 @@
 
 (subtest "Focus on tail of second & third list, mutably decrement second value of third list"
   (let ((xs (list 1 (list 1 2 3 (list 11 22 33))))
-        (fn (lambda (x) (- x 1)))
+        (fn #'1-)
         (l  (compose snd! tail (nth! 2) tail snd!)))
    (is (over l fn xs)
        (list 1 (list 2 3 (list 22 32))))))
 
 (subtest "Focus on tail of second & third list, immutably decrement second value of third list"
   (let ((xs (list 1 (list 1 2 3 (list 11 22 33))))
-        (fn (lambda (x) (- x 1)))
+        (fn #'1-)
         (l  (compose snd tail (nth- 2) tail snd)))
     (is (over l fn xs)
         (list 1 (list 2 3 (list 22 32))))
@@ -123,34 +122,34 @@
 
 (subtest "Mutably apply increment function over value under :A key in association list"
   (let* ((xs (list (cons :A 0) (cons :B 2) (cons :C 3)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (key! :A :alist)))
     (over l fn xs)
     (is 1 (focus l xs))))
 
 (subtest "Mutably apply increment function over value under :A key in property list"
   (let* ((xs (list :A 0 :B 2 :C 3))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (key! :A :plist)))
     (over l fn xs)
     (is 1 (focus l xs))))
 
 (subtest "Immutably apply increment function over value under :A key in association list"
   (let* ((xs '((:A . 0) (:B . 2) (:C . 3)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (key :A :alist)))
     (is 1 (focus l (over l fn xs)))))
 
 (subtest "Immutably apply increment function over value under :A key in property list"
   (let* ((xs '(:A 0 :B 2 :C 3))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (key :A :plist)))
     (is 1 (focus l (over l fn xs)))))
 
 (subtest "Mutably apply decrement function over third value in list, under :A key in hash-table"
   (let* ((ht (make-hash-table))
          (xs (list 24 25 27))
-         (fn (lambda (x) (- x 1)))
+         (fn #'1-)
          (l  (compose (key! :A) (nth! 2))))
     (setf (gethash :A ht) xs)
     (over l fn ht)
@@ -185,7 +184,7 @@
   (let ((htx (make-hash-table))
         (hty (make-hash-table))
         (htz (make-hash-table))
-        (fn  (lambda (x) (+ x 1)))
+        (fn  #'1+)
         (l   (compose (key! :X) (key! :Y) fst! (key! :Z) fst!)))
     (setf (gethash :X htx) hty
           (gethash :Y hty) (list htz)
@@ -197,7 +196,7 @@
   (let ((htx (make-hash-table))
         (hty (make-hash-table))
         (htz (make-hash-table))
-        (fn  (lambda (x) (+ x 1)))
+        (fn  #'1+)
         (l   (compose (key :X) (key :Y) fst (key :Z) fst)))
     (setf (gethash :X htx) hty
           (gethash :Y hty) (list htz)
@@ -211,7 +210,7 @@
   (let* ((ht (alist-hash-table (list (cons :G 7) (cons :H 7) (cons :I 9))))
          (pl (list :D 4 :E ht :F 6))
          (al (list (cons :A 1) (cons :B 2) (cons :C pl)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (compose (key! :C :alist) (key! :E :plist) (key! :H :hash-table))))
     (over l fn al)
     (is 8 (focus l al))))
@@ -220,7 +219,7 @@
   (let* ((ht (alist-hash-table (list (cons :G 7) (cons :H 7) (cons :I 9))))
          (pl (list :D 4 :E ht :F 6))
          (al (list (cons :A 1) (cons :B 2) (cons :C pl)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (compose (key :C :alist) (key :E :plist) (key :H :hash-table))))
     (is 8 (focus l (over l fn al)))
     (is 7 (focus l al))))
@@ -229,7 +228,7 @@
   (let* ((ht (alist-hash-table (list (cons :G 7) (cons :H 7) (cons :I 9))))
          (pl (list :D 4 :E ht :F 6))
          (al (list (cons :A 1) (cons :B 2) (cons :C pl)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (compose (key?! :C) (key?! :E) (key?! :H))))
     (over l fn al)
     (is 8 (focus l al))))
@@ -238,7 +237,7 @@
   (let* ((ht (alist-hash-table (list (cons :G 7) (cons :H 7) (cons :I 9))))
          (pl (list :D 4 :E ht :F 6))
          (al (list (cons :A 1) (cons :B 2) (cons :C pl)))
-         (fn (lambda (x) (+ x 1)))
+         (fn #'1+)
          (l  (compose (key? :C) (key? :E) (key? :H))))
     (is 8 (focus l (over l fn al)))
     (is 7 (focus l al))))
